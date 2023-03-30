dd2479b9f50de96d9521dbc5786f847ff8c2801a
22017147 <ltc,.111992>
2023-03-30 11:07:04 +0800

    feature: 创建第一个页面

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // js 入口文件
+
+import ReactDom from "react-dom/client";
+
+const App = <div>
+  <h1>这是一个react项目!!</h1>
+</div>
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(App)
