d1d4d34ec8663988ed447eefcc722ec0a6ca4a4e
22017147 <ltc,.111992>
2023-03-31 16:06:09 +0800

    fix: 增加protal传送门，指定模态框放在那个位置上，类似vue3的传送门

--- src/components/BackDrop/BackDrop.js
+++ src/components/BackDrop/BackDrop.js
@@ -1,12 +1,17 @@
 
 import './BackDrop.css'
 
+import ReactDom from 'react-dom'
+
 const BackDrop = (props)=>{
-  return (
+
+  const backdrop = document.getElementById('backdrop')
+
+  return ReactDom.createPortal((
     <div className="backDrop">
       {props.children}
     </div>
-  )
+  ),backdrop)
 }
 
 export default BackDrop
